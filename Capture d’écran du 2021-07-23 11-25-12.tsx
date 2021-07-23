ce7703ee235e189f09a838cf9816f571ad24324d
GP-S <deep_blue_@hotmail.fr>
2021-07-23 15:36:46 +0200

    first version

---
+++ Capture d’écran du 2021-07-23 11-25-12.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Capture d’écran du 2021-07-23 11-25-12" tilewidth="805" tileheight="813" tilecount="1" columns="1">
+ <image source="../../Images/Capture d’écran du 2021-07-23 11-25-12.png" width="805" height="813"/>
+</tileset>
---
+++ src/baglogo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="baglogo" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="assets/baglogo.png" width="256" height="64"/>
+</tileset>
