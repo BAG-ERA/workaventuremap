927ea00bceb4eb819246a8d35b9204931d233a9e
GP-S <deep_blue_@hotmail.fr>
2021-07-23 15:47:47 +0200

    fix script

--- src/flologo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="flologo" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="assets/flologo.png" width="64" height="64"/>
-</tileset>
